646d22272266c499db97bd2cc37e93a89d0c7202
riggedCoinflip <tikalker@gmail.com>
2021-04-30 02:43:59 +0200

    purge unused document

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,18 +1,14 @@
 const {SchemaComposer} = require('graphql-compose');
+const {UserQuery, UserMutation} = require('./user');
 
 const schemaComposer = new SchemaComposer();
 
-const {UserQuery, UserMutation} = require('./user.schema');
-const {ProfileQuery, ProfileMutation} = require('./profile.schema');
-
 schemaComposer.Query.addFields({
     ...UserQuery,
-    ...ProfileQuery,
 });
 
 schemaComposer.Mutation.addFields({
     ...UserMutation,
-    ...ProfileMutation,
 });
 
 module.exports = schemaComposer.buildSchema();
