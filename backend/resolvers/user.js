6d559f982ca3035852094ba9cdd2e2edf3cdde21
riggedCoinflip <tikalker@gmail.com>
2021-05-03 13:58:56 +0200

    add (currently faulty) resolver userLogin

--- backend/resolvers/user.js
+++ backend/resolvers/user.js
@@ -1,6 +1,8 @@
 const {UserTC} = require("../models/user");
 const bcrypt = require("bcrypt");
 const {formValid} = require("../../frontend/src/shared/util/validateSignup");
+const {schemaComposer} = require("graphql-compose")
+
 
 /**
  * Generate a bcrypt hash from a plaintext password
@@ -9,7 +11,7 @@ const {formValid} = require("../../frontend/src/shared/util/validateSignup");
  * @param {number} saltRounds 2^saltRounds
  * @returns {(String|null)} hash on succeed, null on error
  */
-async function generateHash(plaintextPassword, saltRounds=10) {
+async function generateHash(plaintextPassword, saltRounds = 10) {
     console.log("hashing")
     try {
         return await bcrypt.hash(plaintextPassword, saltRounds);
@@ -19,7 +21,7 @@ async function generateHash(plaintextPassword, saltRounds=10) {
     }
 }
 
-
+//TODO throw errors
 const createUserOneHashPassword =
     UserTC
         .getResolver('createOne')
@@ -30,9 +32,60 @@ const createUserOneHashPassword =
                 return next(rp);
             } else {
                 console.log("Validation checks failed for:")
-                console.dir(rp)
+                console.dir(rp) //TODO throw error
                 return null;
             }
         })
 
-module.exports = {createUserOneHashPassword}
+//TODO create new document "TOKEN"
+//BUG this code does NOT work RN
+const userLogin = schemaComposer.createResolver({
+    name: "userLogin",
+    kind: "mutation",
+    description: "Provide a valid username + password to login the user",
+    type: UserTC,
+    args: {
+            name: "String",
+            password: "String",
+    },
+
+    resolve: async ({source, args, context, info}) => {
+        console.log(this)
+        let user = await UserTC.getResolver("findOne")
+            .resolve( {
+                    args: {
+                        filter: {
+                            name: args.name
+                        }
+                    }
+                }
+            )
+        console.log(args)
+        console.log(user)
+
+        return await bcrypt
+            .compare(args.password, user.password)
+            .then((valid) => {
+                if (valid) {
+                    return {
+                        name: "foo",
+                        password: "nar"
+                    }//"token" //TODO
+                } else {
+                    return {
+                        name: "",
+                        password: ""
+                    }//""
+                }
+            })
+            .catch((err) => {
+                    console.log(args, user.password)
+                    console.error(err)
+                    return ""
+                }
+            )
+    }
+})
+
+
+module.exports = {createUserOneHashPassword, userLogin}
