71dc9e08a017dd95e3d7639e9f3eabc7e62b7aa1
riggedCoinflip <tikalker@gmail.com>
2021-05-15 16:22:46 +0200

    FIX wrapped home in div tag (was empty HTML tag before)

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,9 +3,9 @@ import React, {Component} from "react";
 export default class Home extends Component{
     render() {
         return (
-        <>
+        <div>
         <h2>Home... </h2>
-        </>
+        </div>
         );
     }
 }
