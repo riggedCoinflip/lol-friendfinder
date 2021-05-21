bd1b373a4d7ceba2d45016ac4d6eb460e894a3ba
riggedCoinflip <tikalker@gmail.com>
2021-05-21 22:28:53 +0200

    instead of dropping the db each time we only delete all users. This fixes tests not passing on the first run and then randomly passing on rerun for some reason... AAAAAAAHHHHHH END ME

--- backend/src/models/user.test.js
+++ backend/src/models/user.test.js
@@ -1,11 +1,13 @@
 const {User} = require("./user.js");
 const validators = require("./user.test.validators");
-const {dbConnect, dbDrop, dbDisconnect} = require("../utils/test-utils/db-handler");
+const {dbConnect, dbClean, dbDisconnect} = require("../utils/test-utils/db-handler");
 const testUsers = require("./user.test.data")
 
 describe("User Model Test Suite", () => {
     beforeAll(async () => dbConnect());
-    afterEach(async () => dbDrop());
+    afterEach(async () => {
+        await User.deleteMany()
+    });
     afterAll(async () => dbDisconnect());
 
     it("saves a user successfully with hashed password", async () => {
@@ -23,34 +25,18 @@ describe("User Model Test Suite", () => {
     })
 
     it("throws MongoDB duplicate error with code 11000", async () => {
-        //FIXME not passing
         const user = new User(testUsers.valid);
         const user2 = new User(testUsers.valid); //clone user
 
         await user.save();
-        /*
+
         try {
             await user2.save();
-            //only works on 2nd run???
             fail("Should throw error");
         } catch (err) {
-            console.log(err)
             const {name, code} = err;
             validators.validateMongoDuplicationError(name, code);
         }
-
-         */
-
-        //why we use init: https://mongoosejs.com/docs/validation.html#the-unique-option-is-not-a-validator
-        await User.init()
-            .then(() => {
-                user2.save()
-                fail("Should throw error");
-            })
-            .catch(err => {
-                const {name, code} = err;
-                validators.validateMongoDuplicationError(name, code);
-            })
     })
 
     it("creates new hash on password change", async () => {
@@ -247,17 +233,14 @@ describe("User Model Test Suite", () => {
     })
 
     it("disallows users with different capitalization", async () => {
-        //FIXME not passing
         const userLower = new User(testUsers.usernameDifferentCapitalization.lower)
         const userUpper = new User(testUsers.usernameDifferentCapitalization.upper)
 
         await userLower.save()
         try {
             await userUpper.save();
-            //FIXME see E11000 duplicate error
             fail("Should throw error");
         } catch (err) {
-            console.log(err)
             const {name, code} = err;
             validators.validateMongoDuplicationError(name, code);
         }
