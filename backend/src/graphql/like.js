fec0669e322eb3031dd25711f26ab542b25a468a
riggedCoinflip <tikalker@gmail.com>
2021-06-04 23:19:40 +0200

    add comments

--- backend/src/graphql/like.js
+++ backend/src/graphql/like.js
@@ -1,12 +1,24 @@
 const {LikeTC} = require("../models/like/like")
 const requireAuthentication = require("../middleware/jwt/requireAuthorization");
 
+//***************
+//*** QUERIES ***
+//***************
+
+//*****************
+//*** MUTATIONS ***
+//*****************
+
 const swipe = LikeTC.mongooseResolvers.createOne()
     .setDescription("Like or Dislike someone. requester gets automatically set and overrides any value")
     .wrapResolve(next => rp => {
         rp.args.record.requester = rp.context.req.user._id
         return next(rp)
     })
+
+//***************
+//*** EXPORTS ***
+//***************
 
 const LikeMutation = {
     ...requireAuthentication({
