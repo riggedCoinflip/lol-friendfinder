1ee154db23e0203c038bed06630dde9ba33adc30
riggedCoinflip <tikalker@gmail.com>
2021-06-06 19:19:59 +0200

    Add pre validate hook, add post save hook

--- backend/src/models/like/like.js
+++ backend/src/models/like/like.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const idvalidator = require('mongoose-id-validator');
+const {User} = require("../user/user");
 const {composeMongoose} = require("graphql-compose-mongoose");
 
 const LikeSchema = new mongoose.Schema({
@@ -22,7 +23,30 @@ LikeSchema.plugin(idvalidator);
 
 LikeSchema.index({requester: 1, recipient: 1}, {unique: true}) //TODO test
 
-const Like = mongoose.model("Match", LikeSchema)
+//Recipient has to differ from Requester
+//TODO test
+LikeSchema.pre("validate", function (next) {
+    if (this.requester.equals(this.recipient)) {
+        this.invalidate("recipient", `Recipient has to differ from Requester "${this.requester}"`, this.recipient)
+    }
+    next()
+})
+
+//TODO test
+LikeSchema.post("save", async function (doc, next) {
+    // if status==="liked" is saved, see if recipient already liked requester.
+    // In this case, add users to each others friends list ♥
+    if (
+        doc.status === "liked" &&
+        await Like.findOne({requester: doc.recipient, recipient: doc.requester, status: doc.status})
+    ) {
+        await User.updateOne({_id: doc.recipient}, {$push: {friends: {user: doc.requester} }})
+        await User.updateOne({_id: doc.requester}, {$push: {friends: {user: doc.recipient} }})
+    }
+    next()
+});
+
+const Like = mongoose.model("Like", LikeSchema)
 const LikeTC = composeMongoose(Like)
 
 module.exports = {
