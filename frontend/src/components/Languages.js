7932b37e72ffbf24374fbdf29a6f6f0bcd4d32eb
Carlo Menji <gilomenjivar@hotmail.com>
2021-06-23 14:28:12 +0200

    Formating, deleting died code

--- frontend/src/components/Languages.js
+++ frontend/src/components/Languages.js
@@ -1,7 +1,6 @@
 import { React, useState, useEffect } from 'react';
-import { useQuery, gql, useMutation } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
 import * as Constants from '../constants'
-//import {selectedLanguages} from './Profile';
 
 import {
   Dropdown, ListGroup, Badge
@@ -17,27 +16,11 @@ const GET_LANGUAGES = gql`
 
   }
 }`;
-
-
-
-const input_alpha2 = "de";
-
-const GET_LANGUAGES_NAME = gql`
-{
-  languageOne(filter: {alpha2: "de" }) {
-    name
-  }
-
-}`;
-
 //ToDo: pass the functions from this component with props
 const Languages = (props) => {
 
   const [local_Languages, setLocal_Languages] = useState([]);
-
   const [searchTerm, setSearchTerm] = useState("");
-
-  //var local_Languages = [] ;//props.state.languages
 
   useEffect(() => {
     setLocal_Languages(props.state.languages);
@@ -45,16 +28,10 @@ const Languages = (props) => {
   }, [props.state.gender])
 
   useEffect(() => {
-    // props.state.languages=local_Languages;
     console.log('useEffect [local_Languages]: ', local_Languages)
-
     props.setState(state => ({ ...state, "languages": local_Languages }));
-
-
     console.log('props.state.languages: ', props.state.languages)
   }, [local_Languages])
-
-
 
   const { loading, error, data } = useQuery(GET_LANGUAGES, {
     context: {
@@ -64,47 +41,26 @@ const Languages = (props) => {
     }
   })
 
-
-
-  const { loadingLanguageName, errorLanguageName, dataLanguageName } =
-    useQuery(GET_LANGUAGES_NAME)
-
   if (loading) return <p>Loading languages...</p>;
-  if (loadingLanguageName) return <p>{console.log('loadingLanguageName:')}</p>;
-
   if (error) return <p>Error!</p>;
-  if (errorLanguageName) return <p>Error errorLanguageName! {console.log('errorLanguageName:')}</p>;
-
-  if (dataLanguageName) return <p>Data {console.log('dataLanguageName:')}...</p>;
 
 
-function alpha2ToName(input)  {
-return data.languageMany.filter(language => {
-  return language.alpha2 === input })
-.map(language => {
-  return language.name
-});
-}
+  //This instead make a Database request to convert alpha2 to Name
+  function alpha2ToName(input) {
+    return data.languageMany.filter(language => {
+      return language.alpha2 === input
+    })
+      .map(language => {
+        return language.name
+      });
+  }
 
+//Test
 console.log('alpha2ToName:', alpha2ToName("en"));
-
-/*
-    var LNames = data.languageMany
-    
-    .filter(item => {
-      return item.alpha2 === "es" })
-    .map(item => {
-      return item.name
-    });
-    console.log('These are the spoken Languages', LNames);
-    */
-    
 
   return (
 
     <div id="avaliableLanguages">
-
-
       <Dropdown>
         <Dropdown.Toggle size="sm" variant="success" id="dropdown-languages">
           Languages
@@ -146,58 +102,33 @@ console.log('alpha2ToName:', alpha2ToName("en"));
       <br />
       <ListGroup horizontal>
         {
-local_Languages &&
-local_Languages
-/*
-.find(item => {
-  return item.map((item, local_Languages) => 
-    {return item.alpha2 === local_Languages} ) 
-})
-*/
+          local_Languages &&
+          local_Languages
+            .map((item, index) => {
+              return (
+                <ListGroup.Item name="spoken-language" value={item} variant="success" key={index + 1} >
+                  {alpha2ToName(item)}
 
-.map((item, index) => {
-  return (
-          /*local_Languages &&
-          local_Languages.map((language, index) => {
-            return (
-              
-              displayedLanguage = displayedLanguage.filter(item => {
-                //find index in language Many
-                // languageMany.name.map
-              })
-              */
+                  <Badge pill variant="danger"
 
-              <ListGroup.Item name="spoken-language" value={item} variant="success" key={index + 1} >
-                {alpha2ToName(item)}
+                    onClick={e => {
+                      e.preventDefault();
+                      const languageToDelete = e.target.parentElement.getAttribute('value');
+                      //console.log('you want to delete', LanguageToDelete )
+                      //Excluding the language we want to delete
+                      setLocal_Languages(local_Languages.filter(item => item !== languageToDelete));
+                      // console.log('Deleting Language: ', local_Languages)
 
-                <Badge pill variant="danger"
-                
-                onClick={e => {
-                  e.preventDefault();
-                  const languageToDelete = e.target.parentElement.getAttribute('value');
-                  //console.log('you want to delete', LanguageToDelete )
-                  //Excluding the language we want to delete
-                  setLocal_Languages(local_Languages.filter(item => item !== languageToDelete));
-                 // console.log('Deleting Language: ', local_Languages)
-
-              }}
-                >
-                  X
+                    }}
+                  >
+                    X
                     </Badge>
-              </ListGroup.Item>
+                </ListGroup.Item>
 
-            );
-          })
+              );
+            })
         }
       </ListGroup>
-
-
-      <p>ToDo: Name of the languages  using localLanguages?
-{
-  /*
-  dataLanguageName.languageOne
-*/}
-      </p>
     </div>
 
   );
