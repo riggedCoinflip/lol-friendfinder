91a9a8e58df70c2888678cffefd63e15d132c329
riggedCoinflip <tikalker@gmail.com>
2021-07-14 19:17:52 +0200

    create get route on "/"

--- backend/src/utils/createExpressApp.js
+++ backend/src/utils/createExpressApp.js
@@ -3,12 +3,16 @@ const isAuth = require("../middleware/jwt/isAuth");
 const routes = require("../routes/avatar")
 
 module.exports = function () {
-    const app = express();
+    const app = express()
 
     //use auth middleware
-    app.use(isAuth);
+    app.use(isAuth)
 
     //mount routes
+    app.get("/", (req, res,next) => {
+        res.send("Hello")
+        next()
+    })
     app.use("/api", routes)
 
     return app
