ecc358819512def5881dd9f6da7e2e4145b64a93
riggedCoinflip <tikalker@gmail.com>
2021-07-17 18:55:30 +0200

    enable playground on prod

--- backend/src/utils/createApolloServer.js
+++ backend/src/utils/createApolloServer.js
@@ -5,6 +5,7 @@ function createApollo(app) {
     const apollo = new ApolloServer({
         schema: graphqlSchema,
         context: ({req, res}) => ({req, res}),
+        introspection: true,
         playground: true
     })
     apollo.applyMiddleware({app, path: "/graphql"})
