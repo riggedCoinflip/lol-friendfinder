0193d3105f690acf81b329032c503e09dcc1e173
Carlo Menji <gilomenjivar@hotmail.com>
2021-08-10 19:42:44 +0200

    Invert conversarion

--- frontend/src/components/ChatMessage.js
+++ frontend/src/components/ChatMessage.js
@@ -1,5 +1,5 @@
-import { useState,  useContext, React } from "react"
-import {  useApolloClient } from "@apollo/client"
+import { useState, useContext, React } from "react"
+import { useApolloClient } from "@apollo/client"
 import { GET_CHAT } from "../GraphQL/Queries"
 import { AuthContext } from "../App"
 import { Headers } from "../constants"
@@ -16,7 +16,7 @@ export default function ChatMessage({ chatID }) {
 
   const { token } = useContext(AuthContext)
   const [conversation, setConversation] = useState()
- // const [conversation2, setConversation2] = useState()
+  // const [conversation2, setConversation2] = useState()
 
   const [participants, setParticipants] = useState()
 
@@ -25,19 +25,19 @@ export default function ChatMessage({ chatID }) {
       context: Headers(token),
       query: GET_CHAT,
       variables: { chatID, page: 1 },
-    //  pollInterval: 100
+      //  pollInterval: 100
     })
   }
 
   GetMessage(chatID).then((res) => {
-  //  console.log("GetConversation", res)
+    //  console.log("GetConversation", res)
     setConversation(res?.data?.getChat?.messages)
     console.log("Conversation(State)", conversation)
-   // console.log("TypeOf: ", typeof conversation)
+    // console.log("TypeOf: ", typeof conversation)
   })
 
-    //to order the messages in the right way
-/*
+  //to order the messages in the right way
+  /*
   function orderC(conversation) {
     setConversation2(Object.assign([], conversation).reverse())
     //setConversation(CInverted)
@@ -46,7 +46,7 @@ export default function ChatMessage({ chatID }) {
   }
   orderC(conversation)
 */
-  // differ between my and message from other users 
+  // differ between my and message from other users
   //let isSentByCurrentUser = false;
 
   // const messageContainer = document.getElementById('oneMsg')
@@ -54,26 +54,29 @@ export default function ChatMessage({ chatID }) {
   return (
     <>
       {conversation &&
-        conversation.map((msg, index) => {
-          /*
+        conversation
+          .slice(0) //with slice(0), we make a copy of the conversation, because this is only-read
+          .reverse() //order of the conversation will be inverted with reverse()
+          .map((msg) => {
+            /*
           if(conversation?.author === state._id){
          messageContainer.style.justifyContent = 'flex-start'
          messageContainer.style.color = 'red'         
         }
          */
 
-          return (
-            <div key={msg?._id} id="oneMsg">
-              <div className="messageContainer justifyEnd">
-                {/*  <p >{msg?.author}</p> */}
-                <div className="">
-                  <p> {msg?.content}/</p>
+            return (
+              <div key={msg?._id} id="oneMsg">
+                <div className="messageContainer justifyEnd">
+                  {/*  <p >{msg?.author}</p> */}
+                  <div className="">
+                    <p> {msg?.content}/</p>
+                  </div>
+                  <p> {msg?.createdAt}</p>
                 </div>
-                <p> {msg?.createdAt}</p>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
     </>
   )
 }
